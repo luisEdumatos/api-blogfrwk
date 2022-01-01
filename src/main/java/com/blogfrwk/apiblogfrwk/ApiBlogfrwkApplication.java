a43a8ce79bccd73b8606b576c7ef4ada62b906c4
luisEdu <luisedu.unifal@gmail.com>
2022-01-01 08:34:07 -0300

    Projeto Spring Criado

---
+++ src/main/java/com/blogfrwk/apiblogfrwk/ApiBlogfrwkApplication.java
@@ -0,0 +1,13 @@
+package com.blogfrwk.apiblogfrwk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBlogfrwkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBlogfrwkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogfrwk/apiblogfrwk/ApiBlogfrwkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogfrwk.apiblogfrwk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBlogfrwkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
