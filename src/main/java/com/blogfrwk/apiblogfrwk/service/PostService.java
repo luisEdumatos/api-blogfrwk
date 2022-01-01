639bf6d5b942923da896f950baf990642f9c06f9
luisEdu <luisedu.unifal@gmail.com>
2022-01-01 14:09:20 -0300

    Adicionado captura de dono do Post

--- src/main/java/com/blogfrwk/apiblogfrwk/service/PostService.java
+++ src/main/java/com/blogfrwk/apiblogfrwk/service/PostService.java
@@ -7,8 +7,12 @@ import com.blogfrwk.apiblogfrwk.dto.response.MessageResponse;
 import com.blogfrwk.apiblogfrwk.entity.Post;
 import com.blogfrwk.apiblogfrwk.exception.PostNotFoundException;
 import com.blogfrwk.apiblogfrwk.repository.PostRepository;
+import com.blogfrwk.apiblogfrwk.security.jwt.JwtUtils;
+import com.blogfrwk.apiblogfrwk.security.services.UserDetailsImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -17,12 +21,21 @@ import java.util.Optional;
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PostService {
     private PostRepository postRepository;
+    private JwtUtils jwtUtils;
+
     private final PostMapper postMapper = PostMapper.INSTANCE;
 
     public MessageResponse createPost(PostDTO postDTO) {
         Post postToSave = postMapper.toModel(postDTO);
+        postToSave.setOwnerName(getPostOwnerName());
         Post savedPost = postRepository.save(postToSave);
         return new MessageResponse("Created Post with ID " + savedPost.getId());
+    }
+
+    private String getPostOwnerName() {
+        Authentication authentication =  SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        return userDetails.getUsername();
     }
 
     public void deleteById(Long id) throws PostNotFoundException {
