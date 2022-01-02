d07f8c44a9ac0a3a694e87a5182428233349e193
luisEdu <luisedu.unifal@gmail.com>
2022-01-02 15:45:26 -0300

    Teste criar e buscar foto por ID

--- src/test/java/com/blogfrwk/apiblogfrwk/PhotoControllerTests.java
+++ src/test/java/com/blogfrwk/apiblogfrwk/PhotoControllerTests.java
@@ -1,6 +1,10 @@
 package com.blogfrwk.apiblogfrwk;
 
 import com.blogfrwk.apiblogfrwk.controller.PhotoController;
+import com.blogfrwk.apiblogfrwk.controller.PostController;
+import com.blogfrwk.apiblogfrwk.dto.request.PhotoDTO;
+import com.blogfrwk.apiblogfrwk.dto.request.PostDTO;
+import com.blogfrwk.apiblogfrwk.entity.Post;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -22,6 +26,9 @@ public class PhotoControllerTests extends ApiBlogfrwkApplicationTests {
     private ObjectMapper objectMapper;
 
     @Autowired
+    private PostController postController;
+
+    @Autowired
     private PhotoController photoController;
 
     @BeforeAll
@@ -30,7 +37,50 @@ public class PhotoControllerTests extends ApiBlogfrwkApplicationTests {
     }
 
     @Test
-    public void testFindAllPhotos() throws Exception {
+    public void testCreatePhoto() throws Exception {
+        PostDTO postDTOMock = new PostDTO();
+        postDTOMock.setDescription("Descricao do Post de Teste");
+
+        this.postController.createPost(postDTOMock);
+
+        Post postMock = new Post();
+        postMock.setDescription("Descricao do Post de Teste");
+        postMock.setId(1L);
+
+        PhotoDTO photoDTOMock = new PhotoDTO();
+        photoDTOMock.setPhotoContent("Conteudo");
+        photoDTOMock.setPhotoPath("Caminho");
+        photoDTOMock.setPost(postMock);
+
+        this.photoMockMvc.perform(MockMvcRequestBuilders.post("/api/photos")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(photoDTOMock)))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+    @Test
+    public void testListAllPhotos() throws Exception {
         this.photoMockMvc.perform(MockMvcRequestBuilders.get("/api/photos")).andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void testFindPhotoById() throws Exception {
+        PostDTO postDTOMock = new PostDTO();
+        postDTOMock.setDescription("Descricao do Post de Teste");
+
+        this.postController.createPost(postDTOMock);
+
+        Post postMock = new Post();
+        postMock.setDescription("Descricao do Post de Teste");
+        postMock.setId(1L);
+
+        PhotoDTO photoDTOMock = new PhotoDTO();
+        photoDTOMock.setPhotoContent("Conteudo");
+        photoDTOMock.setPhotoPath("Caminho");
+        photoDTOMock.setPost(postMock);
+
+        this.photoController.createPhoto(photoDTOMock);
+
+        this.photoMockMvc.perform(MockMvcRequestBuilders.get("/api/photos/1")).andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
