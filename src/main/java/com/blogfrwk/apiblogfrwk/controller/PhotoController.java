2453f9b5cb1eb40223ff12acf465adb08d5745a7
luisEdu <luisedu.unifal@gmail.com>
2022-01-02 15:21:40 -0300

    Create POST Photo com restricao

--- src/main/java/com/blogfrwk/apiblogfrwk/controller/PhotoController.java
+++ src/main/java/com/blogfrwk/apiblogfrwk/controller/PhotoController.java
@@ -1,14 +1,38 @@
 package com.blogfrwk.apiblogfrwk.controller;
 
+import com.blogfrwk.apiblogfrwk.dto.request.PhotoDTO;
+import com.blogfrwk.apiblogfrwk.dto.response.MessageResponse;
+import com.blogfrwk.apiblogfrwk.exception.PhotoCanNotBeCreatedException;
+import com.blogfrwk.apiblogfrwk.exception.PostNotFoundException;
+import com.blogfrwk.apiblogfrwk.service.PhotoService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/photos")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PhotoController {
+
+    private PhotoService photoService;
+
+    @ApiOperation(value = "Cria uma nova Foto no album do Post")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Foto criada com sucesso"),
+            @ApiResponse(code = 400, message = "Erro na validação dos campos da Foto"),
+            @ApiResponse(code = 401, message = "Falha de permissão: Não é possível criar Foto no Post de outro dono / Autenticação de usuário não realizada"),
+            @ApiResponse(code = 404, message = "Erro no parâmetro da requisição Http"),
+    })
+    @PostMapping(consumes="application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponse createPhoto(@RequestBody @Valid PhotoDTO photoDTO) throws PhotoCanNotBeCreatedException, PostNotFoundException {
+        return photoService.createPhoto(photoDTO);
+    }
 }
--- src/main/java/com/blogfrwk/apiblogfrwk/service/PhotoService.java
+++ src/main/java/com/blogfrwk/apiblogfrwk/service/PhotoService.java
@@ -1,10 +1,66 @@
 package com.blogfrwk.apiblogfrwk.service;
 
+import com.blogfrwk.apiblogfrwk.dto.mapper.PhotoMapper;
+import com.blogfrwk.apiblogfrwk.dto.request.PhotoDTO;
+import com.blogfrwk.apiblogfrwk.dto.response.MessageResponse;
+import com.blogfrwk.apiblogfrwk.entity.Photo;
+import com.blogfrwk.apiblogfrwk.entity.Post;
+import com.blogfrwk.apiblogfrwk.exception.PhotoCanNotBeCreatedException;
+import com.blogfrwk.apiblogfrwk.exception.PostNotFoundException;
+import com.blogfrwk.apiblogfrwk.repository.PhotoRepository;
+import com.blogfrwk.apiblogfrwk.repository.PostRepository;
+import com.blogfrwk.apiblogfrwk.security.jwt.JwtUtils;
+import com.blogfrwk.apiblogfrwk.security.services.UserDetailsImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PhotoService {
+
+    private PhotoRepository photoRepository;
+    private PostRepository postRepository;
+    private JwtUtils jwtUtils;
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    private final PhotoMapper photoMapper = PhotoMapper.INSTANCE;
+
+    public MessageResponse createPhoto(PhotoDTO photoDTO) throws PhotoCanNotBeCreatedException, PostNotFoundException {
+        Post userOwnerPost = verifyPostExists(photoDTO.getPost().getId());
+        if(!isCurrentUserOwnsOfThePost(userOwnerPost)) {
+            throw new PhotoCanNotBeCreatedException();
+        }
+        Photo photoToSave = photoMapper.toModel(photoDTO);
+        photoToSave.setCreationDate(sdf.format(new Date()));
+        Photo savedPhoto = photoRepository.save(photoToSave);
+        return new MessageResponse("Create Photo with ID " + savedPhoto.getId());
+    }
+
+    private Post verifyPostExists(Long id) throws PostNotFoundException {
+        Optional<Post> optionalPost = postRepository.findById(id);
+        if (optionalPost.isEmpty()) {
+            throw new PostNotFoundException(id);
+        }
+        return optionalPost.get();
+    }
+
+    private String getUserCurrentSection() {
+        Authentication authentication =  SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        return userDetails.getUsername();
+    }
+
+    public boolean isCurrentUserOwnsOfThePost(Post post) {
+        String userCurrentSection = getUserCurrentSection();
+        if (post.getOwnerName() != null && post.getOwnerName().equals(userCurrentSection)) {
+            return true;
+        }
+        return false;
+    }
 }
